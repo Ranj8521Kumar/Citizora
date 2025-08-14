5dd5bd6a2b3af0457cc3e45b9a870b7baafa4839
Pandit <22it3037@rgipt.ac.in>
2025-08-15 04:04:41 +0530

    Changed backend for reset-password update -1

--- server/src/api/routes/auth.routes.js
+++ server/src/api/routes/auth.routes.js
@@ -37,7 +37,8 @@ router.post('/forgot-password', authController.forgotPassword);
 router.get('/reset-password/:token', (req, res) => {
   const { token } = req.params;
   // Redirect to the frontend reset password page with the token
-  const frontendUrl = process.env.FRONTEND_CITIZEN_URL || 'http://localhost:5173';
+  const frontendUrl = process.env.FRONTEND_CITIZEN_URL;
+  console.log(`frontend url: ${frontendUrl}`);
   res.redirect(`${frontendUrl}/?page=reset-password&token=${token}`);
 });
 
